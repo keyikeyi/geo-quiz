0e9a2023ba34b38a7515a2dda63940a45dda0d77
keyikeyi <tankeyi1997@hotmail.com>
2017-07-12 11:59:49 +0800

    Add play_button, rules_button and exit_button

---
+++ app/src/main/java/com/example/tankeyi/geoquiz/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.tankeyi.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button mPlayButton = (Button) findViewById(R.id.play_button);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, QuizActivity.class));
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/tankeyi/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/tankeyi/geoquiz/QuizActivity.java
@@ -8,7 +8,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
-
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
